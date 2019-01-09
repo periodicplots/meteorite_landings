f6a381f6211bb2b2c57b562ea0f781317f125360
SiderealYear <siderealyear@chaoticgenetics.com>
2019-01-08 19:40:24 -0800

    Clean data
    Exported cleaned data from orignal and 300 to text
    files, no more zeros or empty fields. Year columns
    contain year only.

--- scripts/clean_inspect_data.R
+++ scripts/clean_inspect_data.R
@@ -1,12 +1,23 @@
 # replaced entries with zero mass with NA
 Meteorite_Landings_orignal[Meteorite_Landings_orignal == 0] <- NA
+Meteorite_Landings_300[Meteorite_Landings_300 == 0] <- NA
 # took out any rows with NAs
-#meteorite_landings_300 <- meteorite_landings_300[complete.cases(meteorite_landings_300), ]
+Meteorite_Landings_300 <- Meteorite_Landings_300[complete.cases(meteorite_landings_300), ]
 Meteorite_Landings_orignal <- Meteorite_Landings_orignal[complete.cases(Meteorite_Landings_orignal), ]
+
 # year column contains only year (all days and times Jan 1 at midnight) stripped down to numerical year
 dates <- as.Date(Meteorite_Landings_orignal$year, "%m/%d/%Y")
 years <- year(dates)
 Meteorite_Landings_orignal$year <- years
+
+write.table(x = Meteorite_Landings_orignal, file = "data/Meteorite_Landings_orignal_clean.csv")
+
+dates <- as.Date(Meteorite_Landings_300$year, "%m/%d/%Y")
+years <- year(dates)
+Meteorite_Landings_300 $year <- years
+
+write.table(x = Meteorite_Landings_300, file = "data/Meteorite_Landings_300_clean.csv")
+
 #ggplot(Meteorite_Landings_orignal, aes(x=reclong, y=reclat)) + geom_point()
 #ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram()
 # noted one outlier of ~ 2,000 kg
