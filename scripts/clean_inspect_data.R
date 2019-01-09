4e9473e1f8753f7a4af63fda56d93b013d7692bf
SiderealYear <siderealyear@chaoticgenetics.com>
2019-01-08 16:46:29 -0800

    Started work on data cleaning
    Wrote inital R script to remove rows with banks or zeros. Started
    making some plots to look for outliers etc.

---
+++ scripts/clean_inspect_data.R
@@ -0,0 +1,14 @@
+# replaced entries with zero mass with NA
+Meteorite_Landings_orignal[Meteorite_Landings_orignal == 0] <- NA
+# took out any rows with NAs
+#meteorite_landings_300 <- meteorite_landings_300[complete.cases(meteorite_landings_300), ]
+Meteorite_Landings_orignal <- Meteorite_Landings_orignal[complete.cases(Meteorite_Landings_orignal), ]
+ggplot(Meteorite_Landings_orignal, aes(x=reclong, y=reclat)) + geom_point()
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram()
+# noted one outlier of ~ 2,000 kg
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram() + xlim(0,1.00e+4)
+ggplot(Meteorite_Landings_orignal, aes(x=mass..g.)) + stat_ecdf(geom = "step", pad = FALSE) + xlim(0,1000) + scale_x_continuous(trans='log10') +  annotation_logticks(sides = "b")
+#ggplot(Meteorite_Landings_orignal, aes(x=year)) + stat_ecdf(geom = "step", pad = FALSE)
+
+
+       
