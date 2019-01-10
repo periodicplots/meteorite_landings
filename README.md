dca8a056172c019bfbc61b648ebdad2ccf0aa4ff
SiderealYear <siderealyear@chaoticgenetics.com>
2019-01-09 22:04:51 -0800

    Inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meteorite_landings
+Some fun with a set of 34k meteorite landings curated by Javier de la Torre and the Meteoritical Society. Downloaded 2019-01-08 from NASA's Open Data Portal.
---
+++ scripts/clean_inspect_data.R
@@ -0,0 +1,40 @@
+# replaced entries with zero mass with NA
+Meteorite_Landings_orignal[Meteorite_Landings_orignal == 0] <- NA
+Meteorite_Landings_300[Meteorite_Landings_300 == 0] <- NA
+# took out any rows with NAs
+Meteorite_Landings_300 <- Meteorite_Landings_300[complete.cases(meteorite_landings_300), ]
+Meteorite_Landings_orignal <- Meteorite_Landings_orignal[complete.cases(Meteorite_Landings_orignal), ]
+<<<<<<< HEAD
+ggplot(Meteorite_Landings_orignal, aes(x=reclong, y=reclat)) + geom_point() #+ xlim(-140,-60) + ylim(10,60)
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram()
+# noted one outlier of ~ 2,000 kg
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram() + xlim(0,1.00e+4)
+ggplot(Meteorite_Landings_orignal, aes(x=mass..g.)) + stat_ecdf(geom = "step", pad = FALSE) + xlim(0,1000) + scale_x_continuous(trans='log10') + annotation_logticks(sides = "b")
+ggplot(Meteorite_Landings_orignal, aes(x=year, color=fall)) + stat_ecdf(geom = "step", pad = FALSE) + xlim(1810,2010) #+ scale_y_continuous(trans='log10') + annotation_logticks(sides = "l")
+ggplot(Meteorite_Landings_orignal, aes(x=year, y=mass..g.)) + geom_point() + xlim(1810,2010) + scale_y_continuous(trans='log10') + annotation_logticks(sides = "l")
+ggplot(Meteorite_Landings_orignal, aes(x=mass..g., fill=fall)) + geom_histogram(binwidth = 1, alpha = 0.2) + xlim(0,100) #stat_ecdf(geom = "step", pad = FALSE)  #+ scale_x_continuous(trans='log10') + annotation_logticks(sides = "b")
+=======
+
+# year column contains only year (all days and times Jan 1 at midnight) stripped down to numerical year
+dates <- as.Date(Meteorite_Landings_orignal$year, "%m/%d/%Y")
+years <- year(dates)
+Meteorite_Landings_orignal$year <- years
+
+write.table(x = Meteorite_Landings_orignal, file = "data/Meteorite_Landings_orignal_clean.csv")
+
+dates <- as.Date(Meteorite_Landings_300$year, "%m/%d/%Y")
+years <- year(dates)
+Meteorite_Landings_300 $year <- years
+
+write.table(x = Meteorite_Landings_300, file = "data/Meteorite_Landings_300_clean.csv")
+
+#ggplot(Meteorite_Landings_orignal, aes(x=reclong, y=reclat)) + geom_point()
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram()
+# noted one outlier of ~ 2,000 kg
+#ggplot(meteorite_landings_300, aes(x=mass..g.)) + geom_histogram() + xlim(0,1.00e+4)
+ggplot(Meteorite_Landings_orignal, aes(x=mass..g.)) + stat_ecdf(geom = "step", pad = FALSE) + xlim(0,1000) + scale_x_continuous(trans='log10') +  annotation_logticks(sides = "b")
+ggplot(Meteorite_Landings_orignal, aes(x=year)) + geom_histogram() + xlim(1800,2020)
+
+>>>>>>> 8da7dd4f83799f561c3c44a357712371c4a964b6
+
+       
